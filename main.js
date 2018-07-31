b360e4cb43918acbdf0d5525d2cb55f304fb72d2
budiasa <34809244+budiasa@users.noreply.github.com>
2018-07-31 23:17:57 +0800

    i dont know whats updated

--- main.js
+++ main.js
@@ -47,33 +47,6 @@ var accordion = (function(){
   }
 })();
 
-// video source https://www.videezy.com/random-objects/2313-electric-bulb-hd-stock-video
-
-var container = document.getElementById('container');
-var video = document.getElementById('video-background');
-
-
-// Resize
-function adapt() {
-  var container_width = container.offsetWidth;
-  var container_height = container.offsetHeight;
-
-  video.style.height = "auto";
-  video.style.width = container_width + "px";
-
-  if(video.offsetHeight < container_height) {
-    video.style.height = container_height + "px";
-    video.style.width = "auto";
-  }
-
-  // 處理video置中位置
-  video.style.top = (((video.offsetHeight - container_height) / 2 ) * -1) + 'px';
-  video.style.left = (((video.offsetWidth - container_width) / 2 ) * -1) + 'px';
-}
-
-// events
-window.addEventListener('load', adapt);
-window.addEventListener('resize', adapt);
 
 $(document).ready(function(){
   accordion.init({ speed: 300, oneOpen: true });
